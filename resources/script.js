3eeb5398367cd6210299412701faafe55be4c197
Connor Mahan <63883267+Connor406@users.noreply.github.com>
2020-06-29 15:05:26 -0700

    Initial JS upload for added templating

---
+++ resources/script.js
@@ -0,0 +1,31 @@
+const project = {
+    projects: [
+    {
+        image: './resources/bingo.jpeg',
+        name: 'Bingo App',
+        code: 'https://github.com/Connor406/Bingo',
+        app: 'https://connor406.github.io/Bingo/'
+    },
+    {
+        image: "resources/pages/pine.jpg",
+        name: 'Login Page',
+        code: 'https://github.com/Connor406/Connor406-LoginPage.github.io',
+        app: 'https://connor406.github.io/Connor406-LoginPage.github.io/'
+    },
+    {
+        image: './resources/IMG_3589.jpg',
+        name: 'Site Code',
+        code:'https://github.com/Connor406/Connor406.github.io'
+    }
+    ]
+};
+
+const templateElement = document.getElementById('templateHB');
+
+const templateSource = templateElement.innerHTML;
+
+const template = Handlebars.compile(templateSource);
+
+const compiledHtml = template(project);
+
+document.getElementById('project-container').innerHTML = compiledHtml;
